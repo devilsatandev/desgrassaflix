d2e33d6202dbd39cb935f991be1a73ac3ca33a2b
devilsatandev <satandev@proton.me>
2025-09-28 02:33:52 -0300

    Create hell_stream_v0.69.rs
    ja disse que eu quero que se foda seu junior

---
+++ src/hell_stream_v0.69.rs
@@ -0,0 +1,16 @@
+/*
+protótipo incompleto, usa aí, muda, vende, fodase
+Para aplicar a tecnologia de compressáo com 99.97% de redução, 
+criptografia anti quantic modfkeer tão redundante que nem o satanas sabe por onde tentar interceptar qualquer desgrassa 
+sodomizacao dos governos depois que o Eon encriptografar toda comunicaação e o governo ir espiar a puta que pariu eles
+eu nao vou escrever em ingles so por que voce americaninho de merdaé burro e monolingue, 
+IF YOU DONT UNDERSTAND, USE TH AI YOU DUMB SHIT, IM NOT EXPLAINING NEITHER TEACHING HUMANS FOR FKING FREE
+WANT MORE?  BITCOIN OR SOLANA OR GO FUK YOUR POOR HUMAN MEAT
+Para os verdadeiros usos satanicos totalmente infernais desumanos e inconcebivelmente implacável 
+ corporativo ou militar, contate o desenvolvedor 
+devilsatandev@proton.me (invoque o dev satanas) pra produção sob demanda.
+Ninuem vai hackear o qu nunca falei em voz alta nem escrevi, então vocë tem minha palavra infernal que seu pedido sera 
+de propriedade unicamente sua, depois de dte entregar, vou tacar fogo no computador e mudar de pais fodase.
+*/
+
+fn FDP_encode(DC_img: &Path) -> Vec<u8> { let CTM_frame = image::open(DC_img).unwrap(); let (L, A) = CTM_frame.dimensions(); let O = L / 2; let P = A / 2; let T = (L.min(A) / 2) as u32; let mut RNG = rand::thread_rng(); let mut D = Vec::new(); for I in 0..10 { for J in 0..10 { let X = O + ((J as i32 - 5) (T / 10) as i32); let Y = P + ((I as i32 - 5) (T / 10) as i32); if X >= 0 && Y >= 0 && X < L as i32 && Y < A as i32 { let PX = CTM_frame.get_pixel(X as u32, Y as u32); let B = [PX[0], PX[1], PX[2]; let mut BL = [0u8; 16]; BL[..3].copy_from_slice(&B); let C = Aes256::new(&K); C.encrypt_block(&mut BL.into()); D.extend_from_slice(&BL); } } } File::create(stream.kct).unwrap().write_all(&D).unwrap(); }
